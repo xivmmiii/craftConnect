3e4bf7dc459f74bd707b7241ab96afcbf17f4ba9
Gungun Gupta <gupta.gungun10@gmail.com>
2026-08-13 00:56:42 +0530

    hash the password

--- craftConnect-backend/models/userModel.js
+++ craftConnect-backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -25,6 +26,14 @@ const userSchema = new mongoose.Schema({
     shopName: {
         type: String,
     },
+});
+
+
+////////cannot use arrow function here, because it does not have areference tot his
+userSchema.pre("save", async function (next) { 
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 });
 
 const User = mongoose.model("User", userSchema);
