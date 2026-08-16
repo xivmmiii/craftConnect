05cbef52c4d02cd908717b36d12aa5055544368b
Gungun Gupta <gupta.gungun10@gmail.com>
2026-08-16 22:17:56 +0530

    rename the cart-controller file

---
+++ craftConnect-backend/controllers/cartController.js
@@ -0,0 +1,9 @@
+import Cart from "../models/cartModel.js";
+
+const updateItem = async () => {};
+const viewCart = async (req, res) => {
+    const items=Cart.li
+};
+const addItem = async () => {};
+const removeItem = async () => {};
+const clearCart = async () => {};
