8586f729df07f472521be1fb094a4b0d3f180fd9
Gungun Gupta <gupta.gungun10@gmail.com>
2026-08-17 22:22:40 +0530

    set add to cart controller

--- craftConnect-backend/controllers/cartController.js
+++ craftConnect-backend/controllers/cartController.js
@@ -19,6 +19,49 @@ export const viewCart = async (req, res) => {
         });
     }
 };
-export const addItem = async () => {};
+export const addItem = async (req, res) => {
+    try {
+        const { productID } = req.body;
+        const buyerID = req.user.id;
+        const cart = await Cart.findOne({ buyerID: buyerID });
+
+        if (!cart) {
+            const newCart = await Cart.create({
+                buyerID,
+                items: [{ productID, qty: 1 }],
+            });
+
+            return res.status(200).json({
+                message: "item added",
+                cart: newCart,
+            });
+        } else {
+            const existingItem = cart.items.find(
+                (item) => item.productID.toString() === productID,
+            );
+
+            if (existingItem) {
+                existingItem.qty++;
+            } else {
+                cart.items.push({
+                    productID,
+                    qty: 1,
+                });
+            }
+            await cart.save();
+            return res.status(200).json({
+                message: "item added",
+                cart: cart,
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "internal server error",
+            error: error.message,
+        });
+    }
+};
 export const removeItem = async () => {};
 export const clearCart = async () => {};
+
+
